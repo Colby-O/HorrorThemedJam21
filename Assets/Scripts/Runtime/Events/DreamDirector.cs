5e8b9183c4b5afd3596ff33d28baef6325e943d4
smokeythekat <owr5273@gmail.com>
2025-06-11 14:49:43 -0300

    dreamm

--- Assets/Scripts/Runtime/Events/DreamDirector.cs
+++ Assets/Scripts/Runtime/Events/DreamDirector.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using PlazmaGames.Core;
 using TMPro;
 using UnityEngine;
@@ -7,19 +8,39 @@ namespace HTJ21
     public class DreamDirector : MonoBehaviour
     {
         [SerializeField] private Transform _targetAfterLog;
+        [SerializeField] private TreeFall _sightingTree;
+        [SerializeField] private WalkAndDie _firstCultist;
 
         private IGPSMonoSystem _gpsMs;
+
+        private Dictionary<string, DialogueSO> _dialogues = new();
+
+        private void LoadDialogue(string name)
+        {
+            _dialogues.Add(name, Resources.Load<DialogueSO>($"Dialogue/Dream/{name}"));
+        }
+
+        private void StartDialogue(string name)
+        {
+            if (_dialogues.TryGetValue(name, out DialogueSO d))
+            {
+                GameManager.GetMonoSystem<IDialogueMonoSystem>().Load(d);
+            }
+        }
         
         void Start()
         {
+            LoadDialogue("FallenTree");
             _gpsMs = GameManager.GetMonoSystem<IGPSMonoSystem>();
             GameManager.AddEventListener<Events.DreamFallenLog>(Events.NewDreamFallenLog((from, data) =>
             {
                 _gpsMs.MoveTarget(_targetAfterLog.position);
+                StartDialogue("FallenTree");
             }));
             GameManager.AddEventListener<Events.DreamSighting>(Events.NewDreamSighting((from, data) =>
             {
-                
+                _sightingTree.Fall();
+                _firstCultist.Walk();
             }));
         }
     }
--- Assets/Scripts/Runtime/Events/EventTrigger.cs
+++ Assets/Scripts/Runtime/Events/EventTrigger.cs
@@ -9,9 +9,15 @@ namespace HTJ21
     public class EventTrigger : MonoBehaviour
     {
         [SerializeField] private string _eventName;
+        [SerializeField] private bool _once = true;
+
+        private bool _triggered = false;
 
         private void OnTriggerEnter(Collider other)
         {
+            if (!(other.CompareTag("Player") || other.CompareTag("Car"))) return;
+            if (_triggered && _once) return;
+            _triggered = true;
             System.Type t = typeof(Events).Assembly.GetTypes().FirstOrDefault(t => t.Name == _eventName) ?? typeof(int);
             if (t == typeof(int)) return;
             ConstructorInfo con = t.GetConstructor(Type.EmptyTypes);
