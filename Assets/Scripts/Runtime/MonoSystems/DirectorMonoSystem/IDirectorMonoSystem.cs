801fc60e9d5f5b1ba145f8bdd33202c45644a677
Colby-O <okeefeca@icloud.com>
2025-06-19 14:42:41 -0400

    Director

--- Assets/Scripts/Runtime/MonoSystems/DirectorMonoSystem/IDirectorMonoSystem.cs
+++ Assets/Scripts/Runtime/MonoSystems/DirectorMonoSystem/IDirectorMonoSystem.cs
@@ -5,6 +5,8 @@ namespace HTJ21
 {
     public interface IDirectorMonoSystem : IMonoSystem
     {
-
+        public void NextAct();
+        public Director GetCurrentDirector();
+        public void StartAct(Act act);
     }
 }
